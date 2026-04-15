d9d93bd8b58ab3446d82dcc8d429726b8c95d169
kpt1p_pc <krittapongtip@gmail.com>
2026-04-16 00:03:15 +0700

    fix: failed install lsp server

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,12 +3,12 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        lua_ls = {},
-        emmet_language_server = {},
-        cssls = {},
-        tailwindcss = {},
-        vtsls = {},
-        vue_language_server = {},
+        -- lua_ls = {},
+        -- emmet_language_server = {},
+        -- cssls = {},
+        -- tailwindcss = {},
+        -- vtsls = {},
+        -- vue_language_server = {},
       },
     },
   },
